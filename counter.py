98da1f694e7860411db9004efaeafea7c73a45b4
Jonathan Willis <Jonathan.Willis@vu.com>
2023-04-04 21:04:57 -0500

    9.2 my code

--- counter.py
+++ counter.py
@@ -1,8 +1,22 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
-
-
+    #file_name = 'mbox-short.txt'
+    fhand = open(file_name)
+    dictionary = dict()
+    for line in fhand:
+        if line.startswith('From '):
+            lst = line.split()
+            word = lst[2]
+            if word not in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word]= dictionary[word] +1
+    
+    print(dictionary)
+            
+    fhand.close()
+    
 
 ## if you want to test locally run > python payCalculator.py
 if __name__ == "__main__":
